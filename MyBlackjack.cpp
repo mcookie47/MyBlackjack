f0059c82186ee044fb22a01a48beb4e4de232dbb
mcookie47 <mrgalleta@yahoo.com>
2026-05-20 23:52:48 -0700

    Update MyBlackjack.cpp

--- MyBlackjack.cpp
+++ MyBlackjack.cpp
@@ -4,24 +4,40 @@
 
 using namespace std;
 
-void cards_deck();
+void deck(double card_suites[]);
+int i;
 
 int main()
 {
     //Array for the deck
-    int i;
-    int cards_spade[13], cards_club[13], cards_diamond[13], cards_hearts[13];
-    for (i = 0; i < 13; i++)
-    {
-        cards_spade[i] = i + 1;
-        cout << cards_spade[i] << " ";
-    }
-    
-    for (i = 0; i < 13; i++)
-    {
-        cards_spade[i] = i + 1;
-        cout << cards_spade[i] << " ";
-    }
+
+    double cards_spade[13], cards_club[13], cards_diamond[13], cards_hearts[13];
+
+    cout << "* SUITES *" << endl;
+    cout << "Spades:\t\t";
+    deck(cards_spade);
+    cout << "Clubs:\t\t";
+    deck(cards_club);
+    cout << "Diamond:\t";
+    deck(cards_diamond);
+    cout << "Hearts:\t\t";
+    deck(cards_hearts);
 }
 
-void cards_deck();
+void deck(double card_suites[]) {
+    for (i = 0; i < 13; i++)
+    {
+        card_suites[i] = i + 1;
+        if (i == 0)
+            cout << "Ace" << " ";
+        else if ((i > 0) && (i < 10))
+            cout << card_suites[i] << " ";
+        else if (i == 10)
+            cout << "Jack" << " ";
+        else if (i == 11)
+            cout << "Queen" << " ";
+        else if (i == 12)
+            cout << "King" << " ";
+    }
+    cout << "\n";
+}
