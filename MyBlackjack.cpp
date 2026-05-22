71270d575e7cc37168158cf038561c256d5f9d11
mcookie47 <mrgalleta@yahoo.com>
2026-05-22 00:33:39 -0700

    Update MyBlackjack.cpp

--- MyBlackjack.cpp
+++ MyBlackjack.cpp
@@ -4,8 +4,6 @@
 
 using namespace std;
 
-void deck(double card_suites[]);
-
 void deal();
 
 
@@ -15,38 +13,38 @@ int main()
 {
     //Array for the deck
 
-    double cards_spade[13], cards_club[13], cards_diamond[13], cards_hearts[13];
 
     srand(time(NULL));
 
     int num = (rand() % 13) + 1;
 
-    cout << "* SUITES *" << endl;
-    cout << "Spades:\t\t";
-    deck(cards_spade);
-    cout << "Clubs:\t\t";
-    deck(cards_club);
-    cout << "Diamond:\t";
-    deck(cards_diamond);
-    cout << "Hearts:\t\t";
-    deck(cards_hearts);
     
-    double numbers_suits[4][13];
-    for (int n = 0; n < 4; n++)
+    double suits_numbers[4][13];
+
+    for (int s = 0; s < 4; s++)
     {
-        if (n == 0)
+        if (s == 0)
             cout << "Spades:\t\t";
-        else if (n == 1)
+        else if (s == 1)
             cout << "Clubs:\t\t";
-        else if (n == 2)
+        else if (s == 2)
             cout << "Diamonds:\t";
-        else if (n == 3)
+        else if (s == 3)
             cout << "Hearts:\t\t";
 
-        for (int s = 0; s < 13; s++)
+        for (int n = 0; n < 13; n++)
         {
-            numbers_suits[n][s] = s+1;
-            cout << numbers_suits[n][s] << " ";
+            suits_numbers[s][n] = n+1;
+            if (n == 0)
+                cout << "Ace" << " ";
+            else if ((n >= 1) && (n <= 9))
+                cout << suits_numbers[s][n] << " ";
+            else if (n == 10)
+                cout << "Jack" << " ";
+            else if (n == 11)
+                cout << "Queen" << " ";
+            else if (n == 12)
+                cout << "King" << " ";
         }
         cout << endl;
     }
@@ -55,24 +53,6 @@ int main()
 
     void deal();
 
-}
-
-void deck(double card_suites[]) {
-    for (i = 0; i < 13; i++)
-    {
-        card_suites[i] = i + 1;
-        if (i == 0)
-            cout << "Ace" << " ";
-        else if ((i > 0) && (i < 10))
-            cout << card_suites[i] << " ";
-        else if (i == 10)
-            cout << "Jack" << " ";
-        else if (i == 11)
-            cout << "Queen" << " ";
-        else if (i == 12)
-            cout << "King" << " ";
-    }
-    cout << "\n";
 }
 
 void deal() {
